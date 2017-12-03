35b29c60d967f08543cd7c8960f3048141147e24
unknown <vramkupelian@gmail.com>
2017-12-03 05:09:48 -0800

    My first push from home

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,67 +20,77 @@ var wordPool = ["jefferson","washington","obama","truman","eisenhower","lincoln"
         // This will randomly choose the word for each game
         var theWord = wordPool[Math.floor(Math.random() * wordPool.length)];
 
- var wordPoolLength = wordPool.length;
-
 // This should split word into array with each letter being an index
     var splitWord = theWord.split("");
     var splitLength = splitWord.length;
     
+    var lastDivTiles;
     //HOPEFULLY, splits from array into divs
     for (var w = 0; w < splitLength; w++) {
-        var newElement = document.createElement("div");
-        newElement.id = splitWord[w]; 
-        newElement.className = "blanks";
-        newElement.innerHTML = '-';
-        document.body.appendChild(newElement);
+        lastDivTiles = document.createElement("div");
+        lastDivTiles.id = w; 
+        lastDivTiles.className = splitWord[w];
+        lastDivTiles.innerHTML = '-';
+        document.body.appendChild(lastDivTiles);
     }
     
-
- //   var guessElement = document.getElementById("guesses-left");
- 
- //working on getting a div with an id that matches the user guess
- //var matchId = document.getElementbyId(userGuess);
     
-
-document.onkeyup = function (event) {
+    //Starts on key up
+    document.onkeyup = function (event) {
     
         // Determines which key was pressed.
-        var userGuess = event.key;
-            
+        //var userGuess = event.key;
         
+        //try to convert caps into lowercase
+        var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-    //If user guess is one of the letters
-        
-        
-            if(splitWord.includes(userGuess)){
 
-               console.log(getElementbyId(userGuess)); 
-                for(var i=0; i < splitLength; i++){
+//to find indices of all occurrences of a letter and puts index of each occurrence in array
+var indices = [];
+for(var a = 0; a < splitLength; a++){
+    if(splitWord[a]===userGuess) {
+        indices.push(a);
+    console.log("Pushing into indices");      
+    }
+}
 
-                    
+for(b = 0; b < splitLength; b++){
+
+    if(splitWord[b]===userGuess){
+        if(lastDivTiles.id===indices[b])
+       // lastDivTiles.innerHTML = userGuess;
+    console.log("let's see");
+    }    
+}
+
+//If user's guess is in the word
+if(splitWord.includes(userGuess)){
+    
+                for(var i=0; i < splitLength; i++){       
                 console.log("you've got one");
-
-                 //   }
-                    
-                    
-                   // guessElement.textContent = "You have " + guessesLeft + " left.";
-                    
-              
+                console.log(indices); 
                 }
             }   
+
+            //You lose a guess when you guess incorrectly.
             else{
             guessesLeft = guessesLeft - 1;
             document.getElementById("guesses-left").innerHTML=guessesLeft;
             console.log(guessesLeft);
             }
 
+            // you lose if you run out of guesses
             if (guessesLeft === 0){
-
                 gamesLost = gamesLost + 1;
-                document.getElementById("games-lost").innerHTML=gamesLost;
+                document.getElementById("games-lost").innerHTML=gamesLost;   
             }
 
-
-        
-            console.log(splitWord);
+            //replaces letter but "Uncaught TypeError: Cannot set property 'innerHTML' of null
+            //at HTMLDocument.document.onkeyup"
+            for(c = 0 ; c < splitLength ; c++){
+            var idFind = document.getElementById(indices[c]).innerHTML = userGuess ;
+            console.log("fingers crossed");
+}
+                
+console.log("Made it to end of function");
 }
